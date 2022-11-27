f4078fbffd687871d431b2a9586d50eff4ce07cb
unknown <pguyon69@gmail.com>
2022-11-27 20:06:00 +0100

    up

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import useModal from "./hooks/useModal";
+import Modal from "./components/Modal";
+
+export default function App() {
+  const { open, toggle } = useModal();
+
+  return (
+    <>
+      <div className="App">
+        <button className="modal-toggle" onClick={toggle}>
+          Show modal
+        </button>
+
+        <Modal isOpen={open} hide={toggle} />
+      </div>
+    </>
+  );
+}
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,15 +1,37 @@
-import React, { useState } from 'react'
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+import './Modal.css'
 
-export const Modal = () => {
-    const [open, setOpen] = useState(false)
+const Modal = ({ isOpen, hide }) =>
+  isOpen
+    ? ReactDOM.createPortal(
+        <>
+          <div className="modal__overlay">
+            <div className="modal__wrapper">
+              <div className="modal">
+                <div className="modal__header">
+                  <h4>Modal Header</h4>
+                  <button
+                    type="button"
+                    className="modal__close__button"
+                    onClick={hide}
+                  >
+                    <span>&times;</span>
+                  </button>
+                </div>
+                <div className="modal__body">Hello Modal Here</div>
+              </div>
+            </div>
+          </div>
+        </>,
+        document.body
+      )
+    : null;
 
-    const toggle = () => {
-        setOpen(!open)
-    }
-  return (
-    <>
-    <button onClick={toggle}>Modal</button>
-    {open ? <div>Coucou</div> : null}
-    </>
-  )
-}
+Modal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  hide: PropTypes.func.isRequired,
+};
+
+export default Modal;
